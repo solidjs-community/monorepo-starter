692612a961b0b49448e44c66b87bf888b421b835
thetarnav <thetarnav@users.noreply.github.com>
2023-01-19 10:12:07 +0000

    Format

--- playgrounds/sandbox/src/App.tsx
+++ playgrounds/sandbox/src/App.tsx
@@ -1,31 +1,31 @@
-import type { Component } from "solid-js"
-import { Hello } from "@your-repository-name/hello"
+import type { Component } from 'solid-js'
+import { Hello } from '@your-repository-name/hello'
 
-import logo from "./logo.svg"
-import styles from "./App.module.css"
+import logo from './logo.svg'
+import styles from './App.module.css'
 
 const App: Component = () => {
-	return (
-		<div class={styles.App}>
-			<header class={styles.header}>
-				<img src={logo} class={styles.logo} alt="logo" />
-				<h1>
-					<Hello></Hello>
-				</h1>
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					class={styles.link}
-					href="https://github.com/solidjs/solid"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn Solid
-				</a>
-			</header>
-		</div>
-	)
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <h1>
+          <Hello></Hello>
+        </h1>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App
